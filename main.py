c919421caf9bf85cd725f24e7ec22809c80b5205
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-16 20:02:04 +0300

    Patch embedder and MLP

--- main.py
+++ main.py
@@ -49,3 +49,58 @@ test_dataset = datasets.CIFAR10(root = "data",
                                 download = True,
                                 transform = transform
                                 )
+
+#convert dataset into dataloader
+train_loader = DataLoader(dataset = train_dataset,
+                          batch_size = BATCH_SIZE,
+                          shuffle = True)
+
+test_loader = DataLoader(dataset = test_dataset,
+                         batch_size = BATCH_SIZE,
+                         shuffle = False)
+
+# check dataloader
+print(f"Dataloader: {train_loader,test_loader}")
+print(f"Length of train_loader: {len(train_loader)} batches of {BATCH_SIZE}")
+print(f"Length of test_loader: {len(test_loader)} batches of {BATCH_SIZE}")
+
+
+#the vision transformer
+class PatchEmbedding(nn.Module):
+  def __init__(self,img_size, patch_size,in_channels , embed_dim):
+    super().__init__()
+    self.patch_size = patch_size;
+    self.proj = nn.Conv2d(in_channels = in_channels,
+                          out_channels = embed_dim,
+                          kernel_size = patch_size,
+                          stride = patch_size
+                          )
+    num_patches = (img_size//patch_size)**2
+    self.cls_token = nn.Parameter(torch.randn(1,1,embed_dim))
+    self.pos_embed = nn.Parameter(torch.randn(1,1+num_patches), embed_dim)
+
+
+    def forward(self,x:torch.Tensor):
+      B=x.size(0)
+      x=self.proj(x) #(B,E,H/P,W/P)
+      x=x.flatten(2).transpose(1,2) #(B,N,E)
+      cls_token = self.cls_token(B,-1,-1)
+      x = torch.cat((cls_token,x),dim=1)
+      x=x+self.pos
+      return x
+    
+def MLP(nn.Module):
+  def __init__(self,
+               in_feature,
+               hidden_features.
+               drop_rate):
+    super().__init__()
+    self.fc1 = nn.Linear(in_features = in_feature, out_features = hidden_features)
+    self.fc2 = nn.Linear(in_features=hidden_features,
+                         out_features = in_features)
+    self.dropout = nn.Dropout(drop_rate)
+
+  def forward(self,x):
+    x=self.dropout(F.gelu(self.fc1(x)))
+    x=self.dropout(self.fc2(x))
+    return x
