04ec03e3a243e2508030c2322a83454ab76e7f47
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-16 20:20:20 +0300

    buf fix

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ print(f"Length of test_loader: {len(test_loader)} batches of {BATCH_SIZE}")
 class PatchEmbedding(nn.Module):
   def __init__(self,img_size, patch_size,in_channels , embed_dim):
     super().__init__()
-    self.patch_size = patch_size;
+    self.patch_size = patch_size
     self.proj = nn.Conv2d(in_channels = in_channels,
                           out_channels = embed_dim,
                           kernel_size = patch_size,
@@ -77,25 +77,24 @@ class PatchEmbedding(nn.Module):
                           )
     num_patches = (img_size//patch_size)**2
     self.cls_token = nn.Parameter(torch.randn(1,1,embed_dim))
-    self.pos_embed = nn.Parameter(torch.randn(1,1+num_patches), embed_dim)
+    self.pos_embed = nn.Parameter(torch.randn(1,1+num_patches, embed_dim))
 
-
-    def forward(self,x:torch.Tensor):
-      B=x.size(0)
-      x=self.proj(x) #(B,E,H/P,W/P)
-      x=x.flatten(2).transpose(1,2) #(B,N,E)
-      cls_token = self.cls_token(B,-1,-1)
-      x = torch.cat((cls_token,x),dim=1)
-      x=x+self.pos
-      return x
+  def forward(self,x:torch.Tensor):
+    B=x.size(0)
+    x=self.proj(x) #(B,E,H/P,W/P)
+    x=x.flatten(2).transpose(1,2) #(B,N,E)
+    cls_token = self.cls_token.expand(B,-1,-1)
+    x = torch.cat((cls_token,x),dim=1)
+    x=x+self.pos_embed
+    return x
     
-def MLP(nn.Module):
+class MLP(nn.Module):
   def __init__(self,
-               in_feature,
-               hidden_features.
+               in_features,
+               hidden_features,
                drop_rate):
     super().__init__()
-    self.fc1 = nn.Linear(in_features = in_feature, out_features = hidden_features)
+    self.fc1 = nn.Linear(in_features = in_features, out_features = hidden_features)
     self.fc2 = nn.Linear(in_features=hidden_features,
                          out_features = in_features)
     self.dropout = nn.Dropout(drop_rate)
